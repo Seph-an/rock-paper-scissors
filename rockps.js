c8bc76a3c40101567271e1254781a3db9eb3c1b8
Mhandisi <engsephy@gmail.com>
2022-09-05 18:46:59 +0300

    Completed the rock paper scissors assignment

--- rockps.js
+++ rockps.js
@@ -1,6 +1,6 @@
 // const parameter = document.getElementsByClassName("parameter");
 
-//it should be notted that the result from a 'prompt' is always a string.
+//it should be noted that the result from a 'prompt' is always a string.
 // let playerChoice = prompt("Enter either 'rock', 'paper' or 'scissors'.");
 function getComputerChoice() {
   //with each func call, 'choice' is initialized with a random No. btwn 1 - 10.
@@ -18,46 +18,79 @@ function getComputerChoice() {
   }
   //   return para;
 }
-// getComputerChoice();
-const computerSelection = getComputerChoice();
-// console.log("Computer's choice is", computerSelection);
-const playerSelection = "paper";
-console.log("Player choice:", playerSelection);
-console.log("Computer choice:", computerSelection);
 
-function playRound(p, c) {
+function playRound() {
+  let c = getComputerChoice();
+  let p = prompt(
+    "Enter either 'rock', 'paper' or 'scissors'."
+  ).toLocaleLowerCase();
   //p represents player's choice while c the computer's choice
+  //3-means a tie, 2-means computer has won, 1-means player has won
   if (p === c) {
-    // console.log("It's a tie, do you want to play another round?");
-    let val = "It's a tie, do you want to play another round?";
+    let val = 3;
     return val;
   } else {
-    if (p == "paper" && c == "rock") {
-      //   console.log("You win! Paper covers Rock");
-      let val = "You win! Paper covers Rock";
+    if (
+      (p == "paper" && c == "rock") ||
+      (p == "rock" && c == "scissors") ||
+      (p == "scissors" && c == "paper")
+    ) {
+      let val = 1;
       return val;
-    } else if (p == "paper" && c == "scissors") {
-      //   console.log("You Lose! Scissors cut Paper");
-      let val = "You Lose! Scissors cut Paper";
-      return val;
-    } else if (p == "rock" && c == "scissors") {
-      //   console.log("You Win! Rock crushes Scissors");
-      let val = "You Win! Rock crushes Scissors";
-      return val;
-    } else if (p == "rock" && c == "paper") {
-      //   console.log("You Lose! Paper covers Rock");
-      let val = "You Lose! Paper covers Rock";
-      return val;
-    } else if (p == "scissors" && c == "paper") {
-      //   console.log("You Win! Scissors cuts Paper");
-      let val = "You Win! Scissors cuts Paper";
-      return val;
-    } else if (p == "scissors" && c == "rock") {
-      //   console.log("You Lose! Rock crushes Scissors");
-      let val = "You Lose! Rock crushes Scissors";
+    } else {
+      let val = 2;
       return val;
     }
   }
 }
-const result = playRound(playerSelection, computerSelection);
-console.log(result);
+
+function game() {
+  const outcome = [];
+  for (var i = 0; i < 5; i++) {
+    const result = playRound();
+    const elmt = i + 1;
+    if (result == 1) {
+      console.log("You win round", elmt);
+    } else if (result == 2) {
+      console.log("You lose round", elmt);
+    } else {
+      console.log("Round" + " " + elmt + " is a tie");
+    }
+    // console.log(result);
+    outcome.push(result);
+  }
+  console.log("the scores are:", outcome);
+  mainWinner(outcome);
+}
+
+//loop through the array of scores to add up each score
+function mainWinner(scores) {
+  let playerScore = 0;
+  let compScore = 0;
+  let tieScore = 0;
+  for (let i = 0; i < scores.length; i++) {
+    const element = scores[i];
+    if (element == 1) {
+      playerScore += 1;
+    } else if (element == 2) {
+      compScore += 1;
+    } else {
+      tieScore += 1;
+    }
+  }
+  compareOutCome(playerScore, compScore, tieScore);
+}
+
+//compare the scores of the five round outcome to determine the winner
+function compareOutCome(a, b) {
+  if (a == b) {
+    console.log("It's a tie");
+  } else {
+    if (a > b) {
+      console.log("You are the winner!");
+    } else {
+      console.log("You lost!");
+    }
+  }
+}
+game();
